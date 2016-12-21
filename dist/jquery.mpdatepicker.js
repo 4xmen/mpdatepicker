f4b43cf01b088b447d8161d54381d8c747edb7db
A1Gard <info@pxt.ir>
2016-12-21 10:30:11 +0330

    fix some bug & add time picker

--- dist/jquery.mpdatepicker.js
+++ dist/jquery.mpdatepicker.js
@@ -10,7 +10,7 @@
             datepicker_bg: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAXCAYAAAALHW+jAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AsWCCkyWrAXowAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAJDSURBVDjLnZU9a1RREIaf2b2ra9gFU6g5uLEwIgoBBRWsRFSiweYq/gIbQSsFDVpIQBSsxMKf4AcIcgoRRFGLIGJnExU/ihQeTSlKkt14x2YOHC53N9Epdnlnzpk795135kKFOa+XndcGfcx53ei8TjmvUo7V+ty5AWynvx0BrgKrTgjQGxBbBjTkUpQD4rx2gPdAqxQrBjxQ4/3E9x0YrwGXSsnG7f8k0K1I9ge4BvwGziT+EeB8DdhQunDY+CmANRUJ68BH4CawtxRrifM6BRyqeCVhsFWdeZQBXyr4+1+by4yrU8B8KjXgW1JB2zq7kKhjkzUiNmgzcDt2cSbk0gG2AVuAzyGX0QRfB04DW823C5gDxgyPWkGaJRIh5LJok6AlvAz0Qi5dww2rbCnkouZbUdj9tDfQJ87rXWACeG0PUOv6MyAzPAb8SnjOgAPAS4sXwDHgTmbEzwMPTHfLpq/7wDrDx4EAvLPzTWAP8DAZxYOA4Lzec16fl7bJhxK+4LxOJrjtvH6NvJnvh/N66184lNX4YpcbzusQsNa2TN15zUx/PXv1lvO63pK0je9hU0QvJs+M0N1GcGxKB5ixuVUb/EXgYjLPI8DTpNvDUYd14E3I5WjCx2zIZX+CzwKzIZdXhgX4FHLZl5wJgMSK6iss3iYwlG4VoFb6BNTij8RJGSDYLrCU4AWgiFOSbh9xXqeBc8ALq7QAJoHHQMPwDuCnLYPI/QTwJBndE8AVcV6btix3JpWWd13V7kt9ArwFpv8Ctt/A5Glz+/0AAAAASUVORK5CYII=',
             fontStyle: null,
             gSpliter: '-',
-            complete: null
+            timePicker: null
         }, options);
 
 
@@ -44,7 +44,10 @@
         }
 
 
-
+        this.make2number = function (instr) {
+            var newstr = instr.toString();
+            return newstr.length == 2 ? newstr : '0' + newstr;
+        }
 
         this.gDate2Timestamp = function (stri) {
             return  Math.round(new Date(stri + " 00:00:00").getTime() / 1000);
@@ -52,7 +55,7 @@
 
         this.gTimestamp2Date = function (unix_timestamp) {
             var date = new Date(unix_timestamp * 1000);
-            return date.getFullYear() +  settings.gSpliter  + date.getMonth() + 1 +  settings.gSpliter  + date.getDate();
+            return date.getFullYear() + settings.gSpliter + date.getMonth() + 1 + settings.gSpliter + date.getDate();
         }
         this.pDate2Timestamp = function (stri) {
             return  this.gDate2Timestamp(this.imploiter(this.Persian2Gregorian(this.exploiter(stri))));
@@ -128,7 +131,7 @@
                     cls = cls + ' mp-today';
                 }
                 content = content + ('<td class="' + cls + '"  data-timestamp="' + tmsmp
-                        + '" data-gdate="' + this.imploiter(this.Persian2Gregorian([yr, mn, i]), settings.gSpliter ) + '" title="' +
+                        + '" data-gdate="' + this.imploiter(this.Persian2Gregorian([yr, mn, i]), settings.gSpliter) + '" title="' +
                         this.pTimestamp2Date(tmsmp) + '">' + this.parseHindi(i) + '</td>');
                 if ((i + start_m_weekday) % 7 == 0) {
                     content = content + ('</tr><tr>');
@@ -152,10 +155,20 @@
                 if ($.mpdt.targetPicker.attr('data-gtarget') !== undefined) {
                     $($.mpdt.targetPicker.attr('data-gtarget')).val($(this).attr('data-gdate'))
                 }
+                
+                
+//                console.log($($.mpdt.targetPicker.attr('data-gtarget')).hasClass('mptimepick'));
+                if ($.mpdt.targetPicker.hasClass('mptimepick')) {
+//                    console.log($("#mp-hor").val() + ':' +$("#mp-min").val() + ':' + $("#mp-sec").val() );
+                    $.mpdt.targetPicker.val(  
+                            $.mpdt.make2number(parseInt( $("#mp-hour").val()) ) + ':' +$.mpdt.make2number(parseInt( $("#mp-min").val())) + ':' + $.mpdt.make2number(parseInt( $("#mp-sec").val())) + '  '+
+                            $.mpdt.targetPicker.val()
+                            );
+                }
                 $("#mpdatepicker-modal").fadeOut(200);
             });
 
-           
+
 
             $(".mp-prv").unbind('click.prvmn');
             $(".mp-nxt").unbind('click.nxtmn');
@@ -178,8 +191,8 @@
                 }
                 $.mpdt.ShowMonth($.mpdt.thisMonth + 1, yyyy, $.mpdt.selectedDate);
             });
-            
-             $(".mp-clear").bind('click.clk', function () {
+
+            $(".mp-clear").bind('click.clk', function () {
                 $.mpdt.targetPicker.val('');
                 $.mpdt.selectedDate = '';
                 $("#mpdatepicker-modal").fadeOut(200);
@@ -203,17 +216,21 @@
             $("#mpdatepicker-modal").append('<div id="mpdatepicker-block"><div class="mpbtn mpfleft mp-nxt" >&rsaquo;</div> ' +
                     ' <div class="mpbtn mpfright mp-prv" >&lsaquo;</div><div class="mpheader"><div id="mpmonth"> <ul></ul> <span> اردیبهشت </span>  </div> <div id="mpyear">  <input type="number" value="1396" /> </div>   </div> ' +
                     '<table> <thead> <th> ش </th><th> ی </th><th> د </th><th> س  </th><th> چ </th><th> پ </th><th> ج</th> </thead> <tbody></tbody> </table>' +
-                    '<div class="mp-footer"> <a class="mp-clear"> پاک کردن </a> <a class="mp-today"> امروز </a> <a class="mp-close"> بستن </a> </div></div>');
+                    '<div class="mp-footer"> ' +
+                    '<div class="mptimepicker"> <input value="0" type="number" id="mp-sec"  min="0"  max="59"  /> : '+
+                    ' <input value="0" type="number" id="mp-min"  min="0"  max="59"  />  :'+
+                    ' <input value="0" type="number" id="mp-hour"  min="0"  max="23" /> </div>'
+                    + ' <a class="mp-clear"> پاک کردن </a> <a class="mp-today"> امروز </a> <a class="mp-close"> بستن </a> </div></div>');
 
 //             console.log($("#mpmonth ul li").length);
             // add persian month ro select in cal
-                
-                $(this.persian_month_names).each(function (k, v) {
-                    if (k !== 0  ) {
-                        $("#mpmonth ul").append("<li data-id='" + k + "'>" + this + " </li>");
-                    }
-                });
-                
+
+            $(this.persian_month_names).each(function (k, v) {
+                if (k !== 0) {
+                    $("#mpmonth ul").append("<li data-id='" + k + "'>" + this + " </li>");
+                }
+            });
+
 
             // set select month event
             $("#mpmonth ul li").bind('click.select', function () {
@@ -242,8 +259,8 @@
                 $('body').append('<div id="mpdatepicker-modal" ></div>');
                 $.mpdt.AddDatepcikerBlock();
             }
-            
-            
+
+
 
             $("#mpdatepicker-modal").bind('click.close', function (e) {
                 if ($(e.target).is(this)) {
@@ -385,20 +402,41 @@
         this.each(function () {
             $(this).addClass('mpdatepicker');
 
+            console.log(settings.timePicker);
+            if (settings.timePicker) {
+                $(this).addClass('mptimepick');
+            }
+
 
             $(this).bind('focus.open', function () {
                 $("#mpdatepicker-modal").fadeIn(400);
 
                 if ($(this).val() == '') {
+                    
+
+                    
                     var dtmp = new Date();
                     var today = ($.mpdt.pTimestamp2Date(Math.round(dtmp.getTime() / 1000)));
 //                    $(this).val(today);
                     var vd = $.mpdt.exploiter(today);
-                }else{
+                } else {
+
+                    var newval = $.mpdt.exploiter(' ',$(this).val());
                     
-                    var vd = $.mpdt.exploiter($(this).val());
+                    if (newval.length == 1) {
+                        var currentDay = $(this).val() ;
+                    }else {
+                        var currentDay = newval[1];
+                    }
+                    var vd = $.mpdt.exploiter(currentDay);
                 }
                 
+                if ($(this).hasClass('mptimepick')){
+                    $(".mptimepicker").show();
+                }else{
+                    $(".mptimepicker").hide();
+                }
+
 //                console.log(vd);
                 $.mpdt.ShowMonth(vd[1], vd[0], $(this).val());
                 $.mpdt.selectedDate = $(this).val();
@@ -410,7 +448,7 @@
 
 
             $.mpdt.MakeModalBg();
-            
+
             $.mpdt.WriteCSS();
 
 
